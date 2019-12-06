8b4b76ed95b93c8522668032d9d0bc2d3e78bbde
joao <joaohercilio@hotmail.com>
2019-12-06 10:21:49 -0200

    correto

--- app/src/main/java/com/example/firebaseaulateste/MainActivity.java
+++ app/src/main/java/com/example/firebaseaulateste/MainActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
+    EditText emailText = findViewById(R.id.editText);
+    EditText senhaText = findViewById(R.id.editText2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +30,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+
     }
 
     public void cadastrar(View view) {
-        EditText emailText = findViewById(R.id.editText);
-        EditText senhaText = findViewById(R.id.editText2);
 
         String email = emailText.getText().toString();
         String senha = senhaText.getText().toString();
@@ -43,6 +45,24 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(),"Conta Cadastrado com Sucesso",Toast.LENGTH_LONG).show();
                 }else {
                     Toast.makeText(getApplicationContext(),"Falha ao Cadastrar Conta",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void logar(View view){
+        String email = emailText.getText().toString();
+        String senha = senhaText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Login realizado com sucesso",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, LogadoActivity.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(),"Falha ao realizar login",Toast.LENGTH_LONG).show();
                 }
             }
         });
